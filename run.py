e41c23ce7fb7dc9496143445ce260706b47a7ac9
Fabian Jeske <fabian_jeske@gmx.de>
2022-06-05 23:00:48 +0200

    Adds run file and .idea/ to gitignore

---
+++ run.py
@@ -0,0 +1,21 @@
+"""this cli tool enables the user to track his/her expenses.
+
+useful features:
+- track expenses with category
+- add comment
+- add date
+- get statistics of the expenses (api calls)
+- make predictions based on the expense history
+- also track income
+- track net worth
+- make investment projections
+- display time to retirement based on expenses income and current net worth
+
+
+"""
+
+def main():
+    print("Expense tracker...start your financial journey now!")
+
+if __name__ == "__main__":
+    main()
