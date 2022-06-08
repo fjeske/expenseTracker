d20250cd5cca672ccc23d528ce18e436991257d2
Fabian Jeske <fabian_jeske@gmx.de>
2022-06-08 18:35:03 +0200

    Adds income table and option to track expenses or income

--- run.py
+++ run.py
@@ -16,6 +16,17 @@ import os
 import sqlite3 as db
 from datetime import datetime
 
+def get_table_name(option):
+    """this function gets the cashflow option and returns a str with the table name"""
+    if option.lower() == "e":
+        cf_type = "expenses"
+    elif option.lower() == "i":
+        cf_type = "income"
+    else:
+        raise ValueError("option for cash flow not defined...")
+
+    return cf_type
+
 def init():
     """initialize a database to store expenditures"""
     conn = db.connect("expenses.db")
@@ -29,23 +40,33 @@ def init():
     )
     """
     cur.execute(sql)
+    sql_2 = """
+            CREATE TABLE if not exists income (
+            amount number,
+            category string,
+            message string,
+            date string
+            )
+            """
+    cur.execute(sql_2)
     conn.commit()
     cur.close()
     conn.close()
 
-def log(amount, category, message="", date=None):
+def log(option, amount, category, message="", date=None):
     """
     logs the expenditure in the database
     amount: number
     category: str
     message: (optional) str
     """
+    cf_type = get_table_name(option=option)
     if not date:
         date = str(datetime.now())
     conn = db.connect("expenses.db")
     cur = conn.cursor()
     sql = f"""
-        INSERT into expenses values (
+        INSERT into {cf_type} values (
         {amount},
         \"{category}\",
         \"{message}\",
@@ -57,19 +78,20 @@ def log(amount, category, message="", date=None):
     cur.close()
     conn.close()
 
-def view(category=None):
+def view(option, category=None):
     """
     this functions allows the user to get all entries from the database
     when a existing category is given this functions gives back the expenses for this category
     """
+    cf_type = get_table_name(option=option)
     conn = db.connect("expenses.db")
     cur = conn.cursor()
     if category:
         sql = f"""
-            select * from expenses where category = \"{category}\"
+            select * from {cf_type} where category = \"{category}\"
             """
     else:
-        sql = "select * from expenses"
+        sql = f"select * from {cf_type}"
 
     cur.execute(sql)
     results = cur.fetchall()
@@ -91,17 +113,27 @@ def main():
         if selection.lower() == "q":
             break
         elif selection.lower() == "l":
-            amount = float(input("How much did you spent? "))
+            print("Select category to log to:")
+            print("\t (e) for expenses.")
+            print("\t (i) for income.")
+            cashflow_type = input(">>> ")
+
+            amount = float(input("Enter the amount: "))
             category = str(input("Which category do you want to log? "))
             message = str(input("Enter a message for your entry. "))
             date = str(input("Enter a date (YYYY-MM-DD) if you want, otherwise today will be entered automatically. "))
             if date.strip() == "":
-                log(amount, category, message, date)
+                log(option=cashflow_type, amount=amount, category=category, message=message, date=date)
             else:
-                log(amount, category, message)
+                log(option=cashflow_type, amount=amount, category=category, message=message)
                 # log(125.47, "food", "dinner at a restaurant")
         elif selection.lower() == "v":
-            results = view()
+            print("Select category to view:")
+            print("\t (e) for expenses.")
+            print("\t (i) for income.")
+            cashflow_type = input(">>> ")
+
+            results = view(option=cashflow_type)
             for result in results:
                 print(result)
             input("Press Enter to get back to the main menu. ")
